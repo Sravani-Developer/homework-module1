dad7bac220ea49f45d29ee242a2e12e42fe67bcc
Sravani Medagam <sravanimedagam0709@gmail.com>
2025-09-28 13:51:46 -0700

    created separate JS  files for clock and partners and moved all the code

---
+++ js/site_scripts.js
@@ -0,0 +1,28 @@
+(function renderPartners(){
+    var list = document.getElementById('partners');
+    if (!list) return;
+    var partners = [
+      { src: 'images/partners/partner-bustour.png',      alt: 'Partner Bus Tours' },
+      { src: 'images/partners/partner-cabinrental.png',  alt: 'Partner Cabin Rental' },
+      { src: 'images/partners/partner-campingadv.png',   alt: 'Partner Camping Adventure' },
+      { src: 'images/partners/partner-collegetours.png', alt: 'Partner College Tours' },
+      { src: 'images/partners/partner-rentalbike.png',   alt: 'Partner Bike Rentals' },
+      { src: 'images/partners/partner-tourgroup.png',    alt: 'Partner Tour Group' }
+    ];
+
+    list.innerHTML = ''; 
+
+    partners.forEach(function(p, idx){
+      var li  = document.createElement('li');
+      li.className = 'partner';
+      var img = document.createElement('img');
+      img.src = p.src;
+      img.alt = p.alt;
+      li.appendChild(img);
+      list.appendChild(li);
+
+      if (idx < partners.length - 1) {
+        list.appendChild(document.createTextNode(' , '));
+      }
+    });
+  })();
